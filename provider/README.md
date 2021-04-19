72aaca2a481655a05fbb5716c0069b317402f4a8
Adam Wierzbicki <awierzbicki@golem.network>
2021-04-19 17:09:57 +0200

    Added support for other task types than 'sentiment-analysis'
    in download_model.py and run_classifier.py.
    
    Signed-off-by: Adam Wierzbicki <awierzbicki@golem.network>

--- provider/README.md
+++ provider/README.md
@@ -13,16 +13,20 @@ $ . .venv/bin/activate
 $ pip install -r requirements.txt
 ```
 
-### 1. Download model
+### 1. Download models
 
 ```bash
-$ python download_model.py nateraw/bert-base-uncased-emotion ./models/emotion
+$ python download_model.py sentiment-analysis nateraw/bert-base-uncased-emotion ./models/emotion
+$ python download_model.py summarization sshleifer/distilbart-cnn-12-6 ./models/summary
 ```
 
 ### 2. Run the classifier
 
 ```bash
-$ python run_classifier.py ./models/emotion/ ./the_queen_sample.txt ./output.txt
+$ python run_classifier.py sentiment-analysis ./models/emotion/ ./the_queen_sample.txt ./output.txt
 $ cat output.txt 
 {'sadness': 0.19551274180412292, 'joy': 0.6668532490730286, 'love': 0.061145614832639694, 'anger': 0.051483768969774246, 'fear': 0.02209818735718727, 'surprise': 0.0029064537957310677}
+$ python run_classifier.py summarization ./models/summary/ ./the_queen_sample.txt ./output.txt
+$ cat output.txt 
+The Queen hosted a ceremony in which the Earl Peel stood down as Lord Chamberlain . She accepted her former aide's wand and office insignia at a private event at Windsor Castle . The duke's funeral will take place at Windsor on Saturday . A royal official said members of the family would continue to undertake engagements .
 ```
--- provider/run_classifier.py
+++ provider/run_classifier.py
@@ -4,24 +4,30 @@ from pathlib import Path
 from transformers import pipeline
 
 
-def run(model: str, input_path: Path, output_path: Path) -> None:
-    classifier = pipeline('sentiment-analysis', model=model, return_all_scores=True)
+def run(task: str, model: str, input_path: Path, output_path: Path) -> None:
+    kwargs = {'return_all_scores': True} if task == 'sentiment-analysis' else {}
+    classifier = pipeline(task, model=model, **kwargs)
     text = input_path.read_text(encoding="utf-8")
     result = classifier(text)
-    result_str = str({item['label']: item['score'] for item in result[0]})
+    if task == 'sentiment-analysis':
+        result_str = str({item['label']: item['score'] for item in result[0]})
+    elif task == 'summarization':
+        result_str = result[0]['summary_text']
+    else:
+        result_str = str(result)
     output_path.write_text(result_str, encoding="utf-8")
 
 
 if __name__ == '__main__':
     try:
-        model, input_path, output_path = sys.argv[1:]
+        task, model, input_path, output_path = sys.argv[1:]
         input_path = Path(input_path)
         output_path = Path(output_path)
     except (ValueError, TypeError):
-        print(f"Usage: {sys.argv[0]} <model> <input> <output>")
+        print(f"Usage: {sys.argv[0]} <task> <model> <input> <output>")
         sys.exit(1)
 
     assert input_path.is_file(), f"{input_path} is not a file"
     assert output_path.parent.is_dir(), f"{output_path.parent} is not a directory"
 
-    run(model, input_path, output_path)
+    run(task, model, input_path, output_path)
