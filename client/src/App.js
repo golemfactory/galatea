cbaf7f510ffbf3f574e5c04a7ef3e3811dae5726
Natalia Kulas <nataliakulas@gmail.com>
2021-04-21 15:06:21 +0100

    Simplified server integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,49 @@
-const App = () => (
-  <div>
-    <header>Yagna service PoC</header>
-  </div>
-);
+import { useState } from 'react';
+
+const App = () => {
+  const [text, setText] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setText(e.currentTarget.value);
+  };
+
+  const [result, setResult] = useState(undefined);
+
+  const handleReset = () => setResult(undefined);
+
+  const handleFetch = (e) => {
+    e.preventDefault();
+
+    fetch('http://0.0.0.0:5000/api/classify', {
+      method: 'post',
+      body: new URLSearchParams(`text=${text}`),
+    })
+      .then((response) => response.json())
+      .then(({ results }) => setResult(results))
+      .catch((error) => console.log(error));
+  };
+
+  return result ? (
+    <div className="result">
+      <div>
+        <span>Response is:</span>
+        {result.map((result) => result.summary_text)}
+      </div>
+      <button type="button" onClick={handleReset}>
+        Reset
+      </button>
+    </div>
+  ) : (
+    <form onSubmit={handleFetch}>
+      <label htmlFor="text">What do you want to send?</label>
+      <div>
+        <input name="text" id="text" placeholder="Type something" value={text} onChange={handleChange} />
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
 
 export default App;
