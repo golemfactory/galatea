437f1479f5d7d700699e06a9f0ec58971858dcb1
Paweł Nowosielski <pawel.nowosielski@golem.network>
2021-04-22 12:14:03 +0200

    compose starts api server which waits for yagna ready

--- server/api/src/app.py
+++ server/api/src/app.py
@@ -3,6 +3,23 @@ from flask_cors import CORS
 
 import classifier
 import os
+from pathlib import Path
+from time import sleep, time
+
+# Wait for the `app_key` file is available in `yagna` directory. This file is created by yagna service init script.
+MAX_WAIT_TIME_SECONDS = 60
+wait_start = time()
+path = Path("./yagna/app_key")
+
+print(f"Waiting for Yagna service ready... (allowing {MAX_WAIT_TIME_SECONDS} secs)")
+while not path.exists():
+    sleep(1)
+    assert time() - wait_start < MAX_WAIT_TIME_SECONDS, "Yagna service was not ready in the time period."
+
+YAGNA_APP_KEY = path.read_text().strip()
+print(f"{YAGNA_APP_KEY=}")
+assert len(YAGNA_APP_KEY) == 32, "Unable to read Yagna app key."
+
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
